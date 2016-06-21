06fa033a431c2023e9b21a42a95473a1d6d7079c
Cameron Earl <cameron.l.earl@gmail.com>
2016-06-21 14:32:25 -0700

    created web project

---
+++ Sudoku.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sudoku.Web.Startup))]
+namespace Sudoku.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
